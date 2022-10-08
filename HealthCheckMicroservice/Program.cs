b830c3d103a59472af3b941fed5d2edb4ccc4a9d
bArtOzzzz <breakyourbrain55@gmail.com>
2022-10-08 15:46:51 +0300

    Health check implementation

--- HealthCheckMicroservice/Program.cs
+++ HealthCheckMicroservice/Program.cs
@@ -1,22 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+// Add HealthChecks
+builder.Services.AddHealthChecksUI()
+                .AddInMemoryStorage();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseAuthorization();
+// Add path for endpoints
+app.MapHealthChecksUI();
 
 app.MapControllers();
 
